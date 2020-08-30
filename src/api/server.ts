2791d610322c843a0e5be7fe66354a2d8afd517c
theADAMJR <adamjf123@gmail.com>
2020-08-30 13:51:03 +0100

    there we go

--- src/api/server.ts
+++ src/api/server.ts
@@ -47,9 +47,9 @@ export class API {
         app.get('/server', (req, res) => res.redirect(`https://discord.gg/${config.api.supportInvite}`));
         
         // uncomment if you are using Glitch or client-side rendering
-        /*app.use(express.static(join(__dirname, '../../dist/dashboard')));
+        /*app.use(express.static(join(__dirname, '../../dist/browser')));
         app.all('*', (req, res) => res.status(200).sendFile(
-            join(__dirname, '../../dist/dashboard/index.html')));*/
+            join(__dirname, '../../dist/dashboard/browser.html')));*/
 
         const port = config.api.port || 3000;
         app.listen(port, () => Log.info(`API is live on port ${port}`));
