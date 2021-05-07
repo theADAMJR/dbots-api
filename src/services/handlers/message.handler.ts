71b67536f7d14dbf168138176732a473e72c3ed8
Itzzz-Malik <76797986+Itzzz-Malik@users.noreply.github.com>
2021-05-07 18:13:39 +0300

    Update message.handler.ts

--- src/services/handlers/message.handler.ts
+++ src/services/handlers/message.handler.ts
@@ -11,7 +11,7 @@ export default class MessageHandler implements EventHandler {
     async invoke(msg: Message) {
         if (msg.author.bot) return;
 
-        const prefix = `dbots `;
+        const prefix = `..`;
         const isCommand = msg.content.startsWith(prefix);
         if (isCommand)
             return this.commands.handle(msg, prefix);
